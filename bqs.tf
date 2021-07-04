148d4fdc6970f8c2ce37ddf9f3cc549f5b3ecf6d
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-05 02:24:52 +0530

    Update bqs.tf

--- bqs.tf
+++ bqs.tf
@@ -1,7 +1,7 @@
 module "test-dataset-1" {
   source      = "./modules/bqs"
   dataset_id  = "ind_coe_dataset1"
-  project_id  = ""
+  project_id  = "expanded-origin-316105"
   dataset_location = "US"
   #iam_members = []
 }
