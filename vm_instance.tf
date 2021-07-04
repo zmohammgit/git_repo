055d5693588daaf80db961e903be0e145ff01104
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-05 02:26:08 +0530

    Update vm_instance.tf

--- vm_instance.tf
+++ vm_instance.tf
@@ -1,7 +1,7 @@
 module "vm-instance-1" {
   source        = "./modules/vm_instance"
   name        = "vminstance1"
-  project_id    = ""
+  project_id    = "expanded-origin-316105"
   location        = "US-CENTRAL1"
   #iam_members = []
   zone          = "us-central1-c"
