d6cf916ccd1ac665a2c506fb67e59102f07a085f
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-04 22:02:49 +0530

    Create var.tf

---
+++ modules/service_account/var.tf
@@ -0,0 +1,26 @@
+variable "project_id" {
+        type = string
+        default = ""
+        
+  
+}
+
+variable "display_name" {
+        type = string
+        default = ""
+        
+  
+}
+
+
+variable "project_roles"{
+    type= list(string)
+}
+
+variable "account_id"{
+
+}
+
+#variable "member"{
+
+}
