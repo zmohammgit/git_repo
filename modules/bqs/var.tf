d8ce59272b2fa934205ae45398b532d169c0a637
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-04 21:56:23 +0530

    Create var.tf

---
+++ modules/bqs/var.tf
@@ -0,0 +1,18 @@
+variable "project_id" {
+        type = string
+        #default = "expanded-origin-316105"
+        
+  
+}
+
+
+
+variable "dataset_id" {
+    #type = string
+    #default = [""ind_coe-bucket1","ind_coe-bucket2","ind_coe-bucket3"]
+    #default = "ind_coe-dataset1"
+}
+variable "dataset_location" {
+    type = string
+    default = "us-central1-a"
+}
