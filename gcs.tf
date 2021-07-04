eae6c1f5b298df79b4ebee4ecbfd27650b2a8882
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-05 02:25:20 +0530

    Update gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -2,7 +2,7 @@
 module "test-bucket-1" {
   source      = "./modules/gcs"
   name        = "indcoe_bucket_11"
-  project_id  = ""
+  project_id  = "expanded-origin-316105"
   location    = "US-CENTRAL1"
   #iam_members = []
 }
