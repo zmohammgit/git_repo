8d05760cda4e00e0aae26735b69ee595b92bd5f9
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-05 20:54:44 +0530

    Update main.tf

--- modules/service_account/main.tf
+++ modules/service_account/main.tf
@@ -1,19 +1,4 @@
-provider "template" {
-  version = "~>2.1.2"  #### provider version nor needed anymore with the latest terraform update    #####
-}
 
-provider "google" {
-  version = "=3.23.0"
-  project = var.project_id
- 
-}
-
-provider "google-beta" {
-  version = "=3.23.0"
-  project = var.project_id
-  #region  = var.location
-  #zone    = var.zone
-}
 
 
 resource "google_service_account" "account" {
