51db077d9e53402bcddd38b0a1c1e4027add9e28
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-05 23:38:26 +0530

    Update service_account.tf

--- service_account.tf
+++ service_account.tf
@@ -1,10 +1,12 @@
 module "service_accounts" {
   source        = "./modules/service_account"
-  project_id    = "expanded-origin-316105"
+  project_id    = var.project_id
   project_roles = ["expanded-origin-316105=>roles/viewer"]
-  display_name  = "Serviceaccount"
-  account_id    = "google-service-account"
+  display_name  = var.display_name
+  account_id    = var.account_id
   #member        =  ""
   
 }
+
+
 
