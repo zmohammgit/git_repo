433341fd398fbe49d101262ef0398597ca2b53cc
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-05 20:53:25 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+        type = string
+        default = "expanded-origin-316105"
+        
+  
+}
+
+variable "location" {
+        
+        #description = "specify the region where the instances are to be deployed"
+        #type        =  string
+        default     =  "US-CENTRAL1"
+  
+}
