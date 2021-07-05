fafdd030c1e0d9501d14b3b948889609c0052c9a
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-05 20:52:43 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,29 @@
+
+provider "template" {
+  version = "~>2.1.2"  #### provider version nor needed anymore with the latest terraform update    #####
+}
+
+provider "google" {
+  version = "=3.23.0"
+  project = var.project_id
+  #credentials = "service.json"
+}
+
+provider "google-beta" {
+  version = "=3.23.0"
+  project = var.project_id
+  region  = var.location
+  #zone    = var.zone
+}
+
+terraform {
+ # required_version = "=1.0.0"
+  backend "gcs" {
+    bucket = "buckey-2611"
+    prefix = "terraform/statefile"
+    #credentials = "service.json"
+   
+   
+    
+  }
+}
