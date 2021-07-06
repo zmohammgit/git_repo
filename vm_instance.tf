49da179b9c08a30436d7ab1f3db66d0b5b7045b3
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-06 23:15:08 +0530

    committing files

--- vm_instance.tf
+++ vm_instance.tf
@@ -1,10 +1,100 @@
-module "vm-instance-1" {
-  source        = "./modules/vm_instance"
-  name        =    var.vm_name
-  project_id    =  var.project_id
-  location        = var.location
-  #iam_members = []
-  zone          =  var.zone
-  
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-557"
+}
+
+#tag 
+module "vm_instance2" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-895"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-551"
+}
+
+#tag 
+module "vm_instance2" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-810"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-677"
+}
+
+#tag 
+module "vm_instance2" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-588"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-560"
+}
+
+#tag 
+module "vm_instance2" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-817"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-760"
+}
+
+#tag 
+module "vm_instance2" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-575"
 }
 
