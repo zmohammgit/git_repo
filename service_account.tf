daa202d4d7b1107af2d005d09c35ecdcdbdc602d
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-07 13:02:37 +0530

    committing files

--- service_account.tf
+++ service_account.tf
@@ -1,12 +1,99 @@
-module "service_accounts" {
-  source        = "./modules/service_account"
-  project_id    = var.project_id
-  project_roles = ["secops-316111=>roles/viewer"]
-  display_name  = var.display_name
-  account_id    = var.account_id
-  #member        =  ""
-  
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
 }
 
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
 
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
 
