7567a40079b7f0f93c74ac86ec2da0ecbde6ae29
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-07 13:01:37 +0530

    committing files

---
+++ C:\Users\nitan\Desktop\Git Repo\service_account.tf
@@ -0,0 +1,90 @@
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
