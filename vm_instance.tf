aa8151faa6b4e145ce413c9934c250930153124f
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-07 15:38:40 +0530

    committing files

--- vm_instance.tf
+++ vm_instance.tf
@@ -1,11 +1,19 @@
 #tag 
-module "vm_instance1" {  
-source = "./modules/vm_instance"
-project_id = var.project_id
-location = var.location
-zone = var.zone 
-machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
-name = "vm_instance-615"
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
+}
+
+#tag 
+module "service_account1" {  
+source = "./modules/service_account"
+project_roles = ["expanded-origin-316105=>roles/viewer"]
+display_name  = var.display_name
+account_id    = var.account_id 
+project_id = "secops-316111"
 }
 
 #tag 
@@ -15,16 +23,6 @@ project_id = var.project_id
 location = var.location
 zone = var.zone 
 machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
-name = "vm_instance-600"
-}
-
-#tag 
-module "vm_instance1" {  
-source = "./modules/vm_instance"
-project_id = var.project_id
-location = var.location
-zone = var.zone 
-machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
-name = "vm_instance-828"
+name = "vm_instance-875"
 }
 
