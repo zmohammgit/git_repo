cec8fbaaaddfb4cc300466218709d5ea970f2afc
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-07 14:50:06 +0530

    Delete gcs.tf

--- gcs.tf
+++
@@ -1,8 +0,0 @@
-#tag 
-module "gcs_bucket1" {  
-source = "./modules/gcs" 
-name = "secops-316111-bucket-555" 
-project_id = "secops-316111" 
-location = "us (multiple regions in United States)"
-}
-
