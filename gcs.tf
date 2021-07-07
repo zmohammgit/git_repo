4ab5cb4486b31f33c00f45a28352e840202510c2
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-07 13:02:34 +0530

    committing files

--- gcs.tf
+++ gcs.tf
@@ -1,7 +1,88 @@
-module "test-bucket-1" {
-  source      = "./modules/gcs"
-  name        = var.bucket_name
-  project_id  = var.project_id
-  location    = var.location
-  #iam_members = []
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-628" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
 }
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-890" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-504" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-527" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-872" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-557" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-875" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-654" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-843" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-676" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-685" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
