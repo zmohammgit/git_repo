0e8413f833c3f4dbb975500fb947eba5fb45383a
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-08 21:23:33 +0530

    committing files

--- vm_instance.tf
+++ vm_instance.tf
@@ -118,3 +118,133 @@ machine_typ = "e2-medium(2 vCPU, 4 GB memory)"
 name = "vm_instance-765"
 }
 
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-560"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-538"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-816"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-645"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-519"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-650"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-585"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-794"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-875"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-521"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-669"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-545"
+}
+
+#tag 
+module "vm_instance1" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-565"
+}
+
