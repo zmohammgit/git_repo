cf4d2eb408dc30168c6553b18562e3d870e026ef
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-08 16:35:25 +0530

    committing files

--- gcs.tf
+++ gcs.tf
@@ -1,7 +1,15 @@
 #tag 
 module "gcs_bucket1" {  
 source = "./modules/gcs" 
-name = "secops-316111-bucket-708" 
+name = "secops-316111-bucket-631" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-703" 
 project_id = "secops-316111" 
 location = "us (multiple regions in United States)"
 }
