ba6779b10732d99f54176457a82b4582a900df95
zmohammgit <87143880+zmohammgit@users.noreply.github.com>
2021-07-09 11:17:12 +0530

    Update vm_instance.tf

--- vm_instance.tf
+++ vm_instance.tf
@@ -4,7 +4,7 @@ source = "./modules/vm_instance"
 project_id = var.project_id
 location = var.location
 zone = var.zone 
-machine_typ = "e2-standard-2(2 vCPU, 8 GB memory)" 
+machines_typ = "e2-standard-2(2 vCPU, 8 GB memory)" 
 name = "vm_instance-713"
 }
 
@@ -14,7 +14,7 @@ source = "./modules/vm_instance"
 project_id = var.project_id
 location = var.location
 zone = var.zone 
-machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+machines_typ = "e2-medium(2 vCPU, 4 GB memory)" 
 name = "vm_instance-757"
 }
 
