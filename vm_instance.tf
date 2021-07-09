26b9ddabd371434d60a00bd254547ce39c294dfb
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-09 08:03:22 +0530

    committing files

--- vm_instance.tf
+++ vm_instance.tf
@@ -38,3 +38,13 @@ machine_typ = "e2-standard-2(2 vCPU, 8 GB memory)"
 name = "vm_instance-749"
 }
 
+#tag 
+module "vm_instance2" {  
+source = "./modules/vm_instance"
+project_id = var.project_id
+location = var.location
+zone = var.zone 
+machine_typ = "e2-medium(2 vCPU, 4 GB memory)" 
+name = "vm_instance-809"
+}
+
