63a8bff7d8876bf425284952f5fc95a742ea363a
zmohamm <44280632+zmohamm@users.noreply.github.com>
2021-07-09 08:02:53 +0530

    committing files

--- gcs.tf
+++ gcs.tf
@@ -6,3 +6,11 @@ project_id = "secops-316111"
 location = "us (multiple regions in United States)"
 }
 
+#tag 
+module "gcs_bucket1" {  
+source = "./modules/gcs" 
+name = "secops-316111-bucket-862" 
+project_id = "secops-316111" 
+location = "us (multiple regions in United States)"
+}
+
